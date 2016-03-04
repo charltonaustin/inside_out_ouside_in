85bd29d74b2a3ce2a7e067dd774ee364fa1fca81
charltonaustin <charlton.austin@gmail.com>
2016-03-03 22:40:02 -0500

    Adding in test that covers both input and output from command line.

--- outside_in/test/outside_in/core_test.clj
+++ outside_in/test/outside_in/core_test.clj
@@ -1,7 +1,16 @@
 (ns outside-in.core-test
   (:require [clojure.test :refer :all]
-            [outside-in.core :refer :all]))
+            [outside-in.core :refer :all])
+  (:import (java.io ByteArrayInputStream InputStreamReader BufferedReader)))
+
+;ByteArrayInputStream in = new ByteArrayInputStream("My string".getBytes());
+;System.setIn(in);
+
+(defn write-to-stdin []
+  (let [input-stream (new ByteArrayInputStream (.getBytes "Charlie"))]
+    (System/setIn input-stream)))
 
 (deftest a-test
-  (let [application-output (with-out-str (-main))]
-    (is (= application-output "Hello, World!\n"))))
+  (binding [*in* (new BufferedReader (new InputStreamReader (new ByteArrayInputStream (.getBytes "Charlie!")) "UTF-8"))]
+    (let [application-output (with-out-str (-main))]
+      (is (= application-output "Hello, Charlie!\n")))))
