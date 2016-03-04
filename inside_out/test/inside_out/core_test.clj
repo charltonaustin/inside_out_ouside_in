431a653fc99a5a5ffec15bbfec64a3b47c340ee7
charltonaustin <charlton.austin@gmail.com>
2016-03-03 21:47:32 -0500

    Initial commit with project structure and setup.

---
+++ inside_out/test/inside_out/core_test.clj
@@ -0,0 +1,7 @@
+(ns inside-out.core-test
+  (:require [clojure.test :refer :all]
+            [inside-out.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
---
+++ outside_in/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to outside_in
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
